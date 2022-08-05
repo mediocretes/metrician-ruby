a016fd248f8365f2a02e07205fb1fca3d36792a9
Nathan Acuff <nathan@expectedbehavior.com>
2022-08-04 20:20:05 -0400

    Move redis reporter into a module and prepend it, to avoid prepend vs alias issues

--- lib/metrician/reporters/redis.rb
+++ lib/metrician/reporters/redis.rb
@@ -9,25 +9,25 @@ module Metrician
     end
 
     def instrument
-      return if ::Redis::Client.method_defined?(:call_with_metrician_time)
-      ::Redis::Client.class_eval do
-        def call_with_metrician_time(*args, &blk)
-          start_time = Time.now
-          begin
-            call_without_metrician_time(*args, &blk)
-          ensure
-            duration = (Time.now - start_time).to_f
-            Metrician.gauge(CACHE_METRIC, duration) if Metrician.configuration[:cache][:command][:enabled]
-            if Metrician.configuration[:cache][:command_specific][:enabled]
-              method_name = args[0].is_a?(Array) ? args[0][0] : args[0]
-              Metrician.gauge("#{CACHE_METRIC}.#{method_name}", duration)
-            end
-          end
-        end
-        alias_method :call_without_metrician_time, :call
-        alias_method :call, :call_with_metrician_time
-      end
+      return if ::Redis::Client.ancestors.include?(Metrician::RedisReporterMethods)
+      ::Redis::Client.prepend(Metrician::RedisReporterMethods)
     end
 
+  end
+
+  module RedisReporterMethods
+    def call(*args, &blk)
+      start_time = Time.now
+      begin
+        super
+      ensure
+        duration = (Time.now - start_time).to_f
+        Metrician.gauge(CACHE_METRIC, duration) if Metrician.configuration[:cache][:command][:enabled]
+        if Metrician.configuration[:cache][:command_specific][:enabled]
+          method_name = args[0].is_a?(Array) ? args[0][0] : args[0]
+          Metrician.gauge("#{CACHE_METRIC}.#{method_name}", duration)
+        end
+      end
+    end
   end
 end
